d17645b55b6ef11e80cea44694c91aad58bba699
Anika Christopher <anika.mchristopher@gmail.com>
2020-01-30 03:14:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ class App < Sinatra::Base
     "#{@number_squared}"
   end
   
-  # get '/say/:number/:phrase' do
-  # end
+  get '/say/:number/:phrase' do
+  end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word1 = params[:word1]
