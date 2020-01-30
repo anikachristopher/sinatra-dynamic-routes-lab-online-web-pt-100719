0ef4861a470c9dd3e9589766dee3aa60effa6b8a
Anika Christopher <anika.mchristopher@gmail.com>
2020-01-30 03:02:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get '/reversename/ :name' do
-    @reversed_name = params [:name].reverse
-    "#{@reversed_name}"
+  get '/reversename/:name' do
+    @name_reversed = params [:name].reverse
+    "#{@name_reversed}"
   end
 end
